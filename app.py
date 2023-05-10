bc409e5f35aeba664758c435059449e27d7e21ca
Davide Guidetti <73661177+ilmada99@users.noreply.github.com>
2023-05-10 12:19:17 +0200

    Initial sample python web app

---
+++ app.py
@@ -0,0 +1,14 @@
+#IDEA FROM: https://github.com/tutsplus/create-a-web-app-from-scratch-using-python-flask-and-mysql
+
+from flask import Flask, render_template, json, request, session
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
