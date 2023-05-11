3cf3551cd48c6332f67b45376dc36fc20bcc58ad
Davide Guidetti <davgui99@gmail.com>
2023-05-11 20:50:06 +0200

    increase web app

--- app.py
+++ app.py
@@ -2,13 +2,20 @@
 
 from flask import Flask, render_template, json, request, session
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
+@app.route('/signUp')
+def registration():
+    return render_template('signUp.html')
+
+@app.route('/signIn')
+def login():
+    return render_template('signIn.html')
+
 if __name__ == "__main__":
     app.run()
+    
