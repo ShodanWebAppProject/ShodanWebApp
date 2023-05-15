ed11c7d61510b47f2bb4b84d84878abd9a8d7f49
Davide Guidetti <davgui99@gmail.com>
2023-05-15 16:59:41 +0200

    Update app_server.py

--- app_server.py
+++ app_server.py
@@ -8,31 +8,24 @@ from flask_sock import Sock
 import shodan
 import requests
 
+app = Flask(__name__)
+sock_vuln = Sock(app)
+sock = Sock(app)
 
-if __name__ == "__main__":
-    '''Run application'''
-    app = Flask(__name__)
-    sock_vuln = Sock(app)
-    sock = Sock(app)
-
-    # Session
-    app.config["SESSION_PERMANENT"] = False
-    app.config["SESSION_TYPE"] = "filesystem"
-    Session(app)
-    
-    app.run()
-    
-
+# Session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @sock_vuln.route('/vuln')
-def echo(sock_vuln):
+def echo(socket_vuln):
     '''Web-socket connection for obtain information about ip vuln'''
 
     app.logger.info("Connection accepted")
 
     api = shodan.Shodan(session["shodanid"])
 
-    target = sock_vuln.receive()
+    target = socket_vuln.receive()
 
     dnsResolve = f"https://api.shodan.io/dns/resolve?hostnames={target}&key={session['shodanid']}"
     print("Target: " + target)
@@ -43,22 +36,22 @@ def echo(sock_vuln):
         host = api.host(host_ip)
     except:
         print("Error in resolving IP")
-        sock_vuln.send("<br><h5><b>ERROR RESOLVING IP</b></h5>")
+        socket_vuln.send("<br><h5><b>ERROR RESOLVING IP</b></h5>")
         return
 
     try:
-        sock_vuln.send("<br><h5><b>VULN</b></h5><br>")
+        socket_vuln.send("<br><h5><b>VULN</b></h5><br>")
         for item in host['vulns']:
             CVE = item.replace('!', '')
-            sock_vuln.send('<b>Vulns</b>: %s<br>' % item)
+            socket_vuln.send('<b>Vulns</b>: %s<br>' % item)
             exploits = api.exploits.search(CVE)
             for item in exploits['matches']:
                 if item.get('cve')[0] == CVE:
-                    sock_vuln.send("<br><b>Description:</b><br>"+item.get('description')+"<br><br>")
+                    socket_vuln.send("<br><b>Description:</b><br>"+item.get('description')+"<br><br>")
         return
     except:
         print('An error occurred')
-        sock_vuln.send("<br><h5><b>ERROR IN REQUEST</b></h5>")
+        socket_vuln.send("<br><h5><b>ERROR IN REQUEST</b></h5>")
 
 @app.route('/')
 def main():
@@ -87,11 +80,11 @@ def getshodanid():
     return session["shodanid"]
 
 @sock.route('/gethostinfo')
-def getshostinfo(sock):
+def getshostinfo(socket_info):
     '''Web-socket connection for obtain information about ip info'''
     api = shodan.Shodan(session["shodanid"])
 
-    target = sock.receive()
+    target = socket_info.receive()
 
     print("Target: " + target)
 
@@ -99,14 +92,14 @@ def getshostinfo(sock):
         host = api.host(target)
     except:
         print("Error in resolving IP")
-        sock.send("<br><h5><b>ERROR RESOLVING IP</b></h5>")
+        socket_info.send("<br><h5><b>ERROR RESOLVING IP</b></h5>")
         return
 
     try:
-        sock.send("<br><h5><b>IP INFORMATION</b></h5><br>")
-        sock.send("<b>IP</b>: %s<br>" % host['ip_str'])
-        sock.send("<b>Organization</b>: %s<br>" % host.get('org', 'n/a'))
-        sock.send("<b>Operating System</b>: %s<br>" % host.get('os', 'n/a'))
+        socket_info.send("<br><h5><b>IP INFORMATION</b></h5><br>")
+        socket_info.send("<b>IP</b>: %s<br>" % host['ip_str'])
+        socket_info.send("<b>Organization</b>: %s<br>" % host.get('org', 'n/a'))
+        socket_info.send("<b>Operating System</b>: %s<br>" % host.get('os', 'n/a'))
         return
     except:
         print('An error occured')
@@ -114,3 +107,7 @@ def getshostinfo(sock):
         #sock.close()
         return
 
+if __name__ == "__main__":
+    '''Run application'''
+    app.run()
+    
