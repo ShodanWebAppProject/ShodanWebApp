c922e93813bc67346226991225710f7f2b0b1e58
Davide-Guidetti <davgui99@gmail.com>
2023-05-15 10:51:19 +0200

    Rescruct restApi to pythonApi

--- app.py
+++ app.py
@@ -14,14 +14,16 @@ import requests
 app = Flask(__name__)
 
 # Socket
-sock = Sock(app)
+sockVuln = Sock(app)
+sock= Sock(app)
+
 # Session
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@sock.route('/vuln')
-def echo(sock):
+@sockVuln.route('/vuln')
+def echo(sockVuln):
     app.logger.info("Connection accepted")
 
     # SHODAN_API_KEY = "W9YKu6EZhmfJEuzdu34weobtOf0WoSQC" 
@@ -29,7 +31,7 @@ def echo(sock):
     # target = '167.114.198.227'
    
 
-    target = sock.receive()
+    target = sockVuln.receive()
 
     # print(session["shodanid"])
 
@@ -46,33 +48,33 @@ def echo(sock):
         host = api.host(hostIP)
     except:
         print("Error in resolution IP")
-        sock.send("<br><h5><b>ERROR RESOLUTION IP</b></h5>")
+        sockVuln.send("<br><h5><b>ERROR RESOLUTION IP</b></h5>")
         #sock.close()
         return
 
     try:     
 
         #print("---------------IP INFORMATION---------------")
-        sock.send("<br><h5><b>IP INFORMATION</b></h5><br>")
-        sock.send("<b>IP</b>: %s<br>" % host['ip_str'])
-        sock.send("<b>Organization</b>: %s<br>" % host.get('org', 'n/a'))
-        sock.send("<b>Operating System</b>: %s<br>" % host.get('os', 'n/a'))
+        # sockVuln.send("<br><h5><b>IP INFORMATION</b></h5><br>")
+        # sockVuln.send("<b>IP</b>: %s<br>" % host['ip_str'])
+        # sockVuln.send("<b>Organization</b>: %s<br>" % host.get('org', 'n/a'))
+        # sockVuln.send("<b>Operating System</b>: %s<br>" % host.get('os', 'n/a'))
 
         # Print vuln information
-        sock.send("<br><h5><b>VULN</b></h5><br>")
+        sockVuln.send("<br><h5><b>VULN</b></h5><br>")
         for item in host['vulns']:
             CVE = item.replace('!','')
-            sock.send('<b>Vulns</b>: %s<br>' % item)
+            sockVuln.send('<b>Vulns</b>: %s<br>' % item)
             exploits = api.exploits.search(CVE)
             for item in exploits['matches']:
                 if item.get('cve')[0] == CVE:
-                    sock.send("<br><b>Desciption:</b><br>"+item.get('description')+"<br><br>")
+                    sockVuln.send("<br><b>Desciption:</b><br>"+item.get('description')+"<br><br>")
 
-        sock.close()
+        # sock.close()
         return
     except: 
         print('An error occured')
-        sock.send("<br><h5><b>ERROR IN REQUEST</b></h5>")
+        sockVuln.send("<br><h5><b>ERROR IN REQUEST</b></h5>")
         #sock.close()
         return
 
@@ -98,16 +100,48 @@ def login():
     return render_template("login.html")
 
 @app.route('/getshodanid/')
-def get():
-    return session.get('shodanid', 'not set')
+def getshodanid():
+    return session["shodanid"]
 
-@app.route('/signUp')
-def signup():
-    return render_template('signUp.html')
+@sock.route('/gethostinfo')
+def getshostinfo(sock):
+    # SHODAN_API_KEY = "W9YKu6EZhmfJEuzdu34weobtOf0WoSQC" 
+    api = shodan.Shodan(session["shodanid"])
+    # target = '167.114.198.227'
 
-@app.route('/signIn')
-def signin():
-    return render_template('signIn.html')
+    target = sock.receive()
+    # print(session["shodanid"])
+
+    #dnsResolve = 'https://api.shodan.io/shodan/host/'+target+'?key='+session["shodanid"]
+
+    print("Target: "+target)
+    
+    try:
+        # First we need to resolve our targets domain to an IP
+        # resolved = requests.get(dnsResolve)
+
+        # hostIP = resolved.json()[target]
+        print(target)
+        # Then we need to do a Shodan search on that IP
+        host = api.host(target)
+    except:
+        print("Error in resolution IP")
+        sock.send("<br><h5><b>ERROR RESOLUTION IP</b></h5>")
+        #sock.close()
+        return
+
+    try:     
+        #print("---------------IP INFORMATION---------------")
+        sock.send("<br><h5><b>IP INFORMATION</b></h5><br>")
+        sock.send("<b>IP</b>: %s<br>" % host['ip_str'])
+        sock.send("<b>Organization</b>: %s<br>" % host.get('org', 'n/a'))
+        sock.send("<b>Operating System</b>: %s<br>" % host.get('os', 'n/a'))
+        return
+    except: 
+        print('An error occured')
+        sock.send("<br><h5><b>ERROR IN REQUEST</b></h5>")
+        #sock.close()
+        return
 
 if __name__ == "__main__":
     app.run()
