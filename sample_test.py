e7930927656b1bbab6da109044db5188b298a03c
Davide Guidetti <davgui99@gmail.com>
2023-05-15 17:26:22 +0200

    Update sample_test.py

--- sample_test.py
+++ sample_test.py
@@ -8,6 +8,10 @@ def app():
     '''fixture app'''
     yield flask_app
 
+@pytest.fixture
+def client(application):
+    '''fixture client'''
+    return application.test_client()
 
 def test_index(client):
     '''test app index'''
@@ -25,9 +29,9 @@ def test_session_key(client):
         assert session.get("shodanid") == "example_key"
 
 
-def test_login(client_user):
+def test_login(client):
     '''test app login'''
-    with client_user.session_transaction() as session:
-        response = client_user.get("/login")
+    with client.session_transaction() as session:
+        response = client.get("/login")
         assert response.status_code == 200
         assert session.get("shodanid") is None
