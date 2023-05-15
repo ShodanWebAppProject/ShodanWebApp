1a2a7468b7d9fbe2a849fbb34753e5e2f3793092
Davide-Guidetti <davgui99@gmail.com>
2023-05-15 16:06:25 +0200

    test

--- sample_test.py
+++ sample_test.py
@@ -14,17 +14,3 @@ def test_index(client):
     response = client.get("/login")
 
     assert response.status_code == 200
-
-def test_session_key(client):
-    with client.session_transaction() as session:
-        session["shodanid"] = "example_key"
-
-    response = client.get("/")
-    assert response.status_code == 200
-    assert session.get("shodanid") == "example_key"
-
-
-def test_login(client):
-    with client.session_transaction() as session:
-        response = client.get("/login")
-    assert session.get("shodanid") == None
