29d4842072c9b11f58a727d632e80e40b91721dd
Marco Motamed <marco.motamed@gmail.com>
2023-07-04 16:27:15 +0200

    Update Terraform files

--- TerraformFiles/main.tf
+++ TerraformFiles/main.tf
@@ -1,15 +1,6 @@
-# Generate random resource group name
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
-
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
-}
-
-resource "random_pet" "azurerm_kubernetes_cluster_name" {
-  prefix = "cluster"
+  name     = var.azurerm_resource_group_name 
 }
 
 resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
@@ -18,7 +9,7 @@ resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
 
 resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
-  name                = random_pet.azurerm_kubernetes_cluster_name.id
+  name                = var.azurerm_kubernetes_cluster_name
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
 
--- TerraformFiles/ssh.tf
+++ TerraformFiles/ssh.tf
@@ -6,7 +6,7 @@ resource "random_pet" "ssh_key_name" {
 resource "azapi_resource" "ssh_public_key" {
   type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   name      = random_pet.ssh_key_name.id
-  location  = "westus3"
+  location  = "northeurope"
   parent_id = azurerm_resource_group.rg.id
 }
 
